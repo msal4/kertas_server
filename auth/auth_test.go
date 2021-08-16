c462d6579cdb33d546121cce449e690d5ea0e493
Mohammed Salman <msal4@outlook.com>
2021-08-16 17:01:05 +0300

    Test auth GenerateToken thoroughly

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
 	"github.com/msal4/hassah_school_server/auth"
 	"github.com/msal4/hassah_school_server/ent"
@@ -14,15 +15,36 @@ import (
 )
 
 func TestGenerateToken(t *testing.T) {
+	cfg := auth.AuthConfig{}
+
 	u := ent.User{
-		ID:   uuid.New(),
-		Role: user.RoleSuperAdmin,
+		ID:           uuid.New(),
+		Role:         user.RoleSuperAdmin,
+		TokenVersion: 20,
 	}
 
-	data, err := auth.GenerateTokens(u, auth.AuthConfig{})
+	data, err := auth.GenerateTokens(u, cfg)
 	require.NoError(t, err)
 	require.NotEmpty(t, data.AccessToken)
 	require.NotEmpty(t, data.RefreshToken)
+
+	var accessClaims auth.AccessClaims
+	_, err = jwt.ParseWithClaims(data.AccessToken, &accessClaims, func(t *jwt.Token) (interface{}, error) {
+		return cfg.AccessSecretKey, nil
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, u.ID, accessClaims.UserID)
+	require.Equal(t, u.Role, accessClaims.Role)
+
+	var refreshClaims auth.RefreshClaims
+	_, err = jwt.ParseWithClaims(data.AccessToken, &refreshClaims, func(t *jwt.Token) (interface{}, error) {
+		return cfg.RefreshSecretKey, nil
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, u.ID, refreshClaims.UserID)
+	require.Equal(t, u.TokenVersion, refreshClaims.TokenVersion)
 }
 
 func TestMiddleware(t *testing.T) {
